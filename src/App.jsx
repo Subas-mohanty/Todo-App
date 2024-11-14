a749d6bcd7c4db7137d5813122cac7b64607ff11
subas <subasmohanty256@gmail.com>
2024-11-14 18:57:16 +0530

    feat: todo add, update, delete functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react";
+import TodoForm from "./components/TodoForm.jsx";
+import TodoItem from "./components/TodoItem.jsx";
+import { TodoContextProvider } from "./contexts";
+
 function App() {
+  const [todos, setTodos] = useState([]);
   
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((currTodo) => currTodo.id === id ? todo : currTodo));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleCompleted = (id) => {
+    setTodos((prev) => 
+      prev.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo)
+    );
+  };
+  
+  return (
+    <TodoContextProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}>
+      <div className="bg-[rgb(23,40,66)] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {(searchTerm ? filteredTodos : todos).map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoContextProvider>
+  );
 }
 
-export default App
+export default App;
